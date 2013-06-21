f15f9998604e88d05c2bae78dfa9b88e77de7c23
Joseph Cottam <jcottam@indiana.edu>
2013-06-21 10:18:36 -0400

    Draft drawing-the-dark.

--- AbstractRendering/app/ar/app/util/WrappedTransfer.java
+++ AbstractRendering/app/ar/app/util/WrappedTransfer.java
@@ -5,7 +5,9 @@ import java.awt.Color;
 
 import javax.swing.JFrame;
 
+import ar.Aggregates;
 import ar.Transfer;
+import ar.aggregates.FlatAggregates;
 import ar.app.ARApp;
 import ar.app.components.ScatterControl;
 import ar.rules.Aggregators;
@@ -135,4 +137,71 @@ public interface WrappedTransfer<A> {
 		public void selected(ARApp app) {}
 		public void deselected() {}
 	}
+	
+	public class DrawDarkVar implements WrappedTransfer<Integer> {
+		private int distance;
+		public DrawDarkVar() {this(10);}
+		public DrawDarkVar(int distance) {this.distance = distance;}
+		public Transfer<Integer> op() {return new DrawDark(Color.BLACK, Color.white, distance);}
+		public Class<Integer> type() {return Integer.class;}
+		public String toString() {return String.format("Draw Dark (%d)", distance);}
+		public void selected(ARApp app) {}
+		public void deselected() {}
+		
+		private class DrawDark implements Transfer<Integer> {
+			final int distance;
+			final Transfer<Integer> inner;
+			Aggregates<Integer> cached;
+			Aggregates<Integer> cacheKey;
+			
+			public DrawDark(Color low, Color high, int distance) {
+				this.distance=distance;
+				inner = new Transfers.Interpolate(low,high);
+			}
+
+			public Color at(int x, int y, Aggregates<Integer> aggregates) {
+				if (cacheKey == null || cacheKey != aggregates) {
+					preproc(aggregates); cacheKey=aggregates;
+				}
+				return inner.at(x,y,cached);
+			}
+			
+			private void preproc(Aggregates<Integer> aggs) {
+				Aggregates<Integer> out = new FlatAggregates<>(aggs.lowX(), aggs.lowY(), aggs.highX(), aggs.highY(), 0);
+				
+				for (int x=aggs.lowX(); x <aggs.highX(); x++) {
+					for (int y=aggs.lowY(); y<aggs.highY(); y++) {
+						if (aggs.at(x, y) >0) {
+							out.set(x, y, preprocOne(x,y,aggs));
+						} else {
+							out.set(x,y, 0);
+						}
+					}
+				}
+				this.cached = out;
+			}
+			
+			private int preprocOne(int x, int y, Aggregates<Integer> aggregates) {
+				int surroundingSum =0;
+				for (int d=-distance; d<=distance; d++) {
+					for (int dx=0; dx<=d; dx++) {
+						for (int dy=0; dy<=d; dy++) {
+							int cx=x+dx;
+							int cy=y+dy;
+							if (cx < aggregates.lowX() || cy < aggregates.lowY() || cx>aggregates.highX() || cy> aggregates.highY()) {continue;}
+							double dv = aggregates.at(cx,cy).doubleValue();
+							if (dv != 0) {surroundingSum++;}
+						}
+					}
+				}
+				return surroundingSum;
+
+			}
+			
+		}
+	}
+	
+	
+	
+	
 }
