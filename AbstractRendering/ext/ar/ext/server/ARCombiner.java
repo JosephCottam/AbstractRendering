3fc799adcb8740d06571a382364cbb3b00591627
Joseph Cottam <jcottam@indiana.edu>
2013-07-01 14:25:30 -0400

    ARCombiner with listener queue.

--- AbstractRendering/ext/ar/ext/server/ARCombiner.java
+++ AbstractRendering/ext/ar/ext/server/ARCombiner.java
@@ -6,8 +6,11 @@ import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.avro.generic.GenericRecord;
 
@@ -26,28 +29,37 @@ import ar.util.Util;
  * This implementation does not "know" how many parts to expect, so it cannot tell if
  * an aggregate set is "done".  However, it counts the number of items combined and can report that.
  * 
+ * When a new aggregate arrives, listeners can be notified.  Alternatively, polling can 
+ * be achieved by monitoring the "count" method's value. 
+ * 
  * This is NOT a compliment to the ARServer.  The ARServer is a REST application that expects incoming
  * socket connections.  Use the ar.avro.AggregatesSerailizer.deserialize methods to handle
  * the results returned from ARServer.
  * 
  */
 public class ARCombiner<A> {
-	private final Listener<A> listener;
+	private final Listener listener;
 	private final Thread listenerThread;
-	private final Queue<Aggregates<A>> incomming;
+	private final Queue<Aggregates<A>> queue;
 	private Aggregates<A> aggs;
-	private int combinedCount=0;
+	protected AtomicInteger count = new AtomicInteger(0);
 	private AggregateReducer<A,A,A> reducer;
-
+	private final List<ArrivalListener> listeners = new ArrayList<ArrivalListener>();
+	
 	public ARCombiner(String hostname, int port, Valuer<GenericRecord,A> converter, AggregateReducer<A,A,A> reducer) throws IOException {
-		this.incomming = new ConcurrentLinkedQueue<Aggregates<A>>();
-		this.listener = new Listener<A>(hostname, port, incomming, converter);
+		this.queue = new ConcurrentLinkedQueue<Aggregates<A>>();
+		this.listener = new Listener(hostname, port, converter);
 		this.reducer = reducer;
 		listenerThread = new Thread(listener);
 		listenerThread.setDaemon(true);
 	}
 	
 	public void start() {listenerThread.start();}
+	protected void addToQueue(Aggregates<A> a) {
+		queue.add(a);
+		count.incrementAndGet();
+		notifyListeners();
+	}
 	
 	/**Signal that the server sould stop.*/
 	public void stop() {
@@ -63,31 +75,32 @@ public class ARCombiner<A> {
 	
 	//TODO: More eager combining...perhaps use another thread and condition variable to awake when there is actual work.
 	public Aggregates<A> combined() {
-		while (!incomming.isEmpty()) {
-			Aggregates<A> item = incomming.poll();
+		while (!queue.isEmpty()) {
+			Aggregates<A> item = queue.poll();
 			if (item != null) {
 				aggs = Util.reduceAggregates(aggs, item, reducer);
-				combinedCount++;
 			}
 		}
 		return aggs;
 	}
 	
-	public int combinedCount() {return combinedCount;}
+	/**How many items have arrived at this combiner?
+	 * This is not the number of pending items, but the total number that have arrived, 
+	 * regardless of their status as pending combining or not.
+	 */
+	public int count() {return count.get();}
 	
-	public static class Listener<A> implements Runnable {
+	public class Listener implements Runnable {
 		public final int port;
 		public final String hostname;
 		private final ServerSocket serverSocket;
-		private final Queue<Aggregates<A>> queue;
 		private final Valuer<GenericRecord,A> elementConverter;
 		private boolean running = false;
 		
-		public Listener(String hostname, int port, Queue<Aggregates<A>> queue, Valuer<GenericRecord, A> converter) throws IOException {
+		public Listener(String hostname, int port, Valuer<GenericRecord, A> converter) throws IOException {
 			this.port = port;
 			this.hostname = hostname;
 			elementConverter = converter;
-			this.queue = queue;
 
 			serverSocket = new ServerSocket();
 			serverSocket.bind((hostname != null) ? new InetSocketAddress(hostname, port) : new InetSocketAddress(port));
@@ -107,8 +120,8 @@ public class ARCombiner<A> {
 	                if (inputStream == null) {safeClose(finalAccept);}
 	                
 	                //TODO: Lazier deserialization (don't tie up the server here)
-	                Aggregates<A> res = AggregateSerailizer.deserialize(inputStream, elementConverter);
-	                queue.add(res);
+	                Aggregates<A> aggs = AggregateSerailizer.deserialize(inputStream, elementConverter);
+	                ARCombiner.this.addToQueue(aggs);
 				} catch (Exception e) {
 					//Ignore all errors if server socket is closed
 					if (!serverSocket.isClosed()) {e.printStackTrace();}
@@ -120,7 +133,7 @@ public class ARCombiner<A> {
 		}
 			
 		public boolean running() {return running;}
-	    private static final void safeClose(Closeable thing) {
+	    private final void safeClose(Closeable thing) {
 	        if (thing != null) {
 	            try {
 	            	thing.close();
@@ -131,5 +144,29 @@ public class ARCombiner<A> {
 	    }
 	}
 	
+	
+	
+	//----------------  Event Listener Support --------------------------------  
+	/**Add a listener to be informed when a new aggregate has arrived.**/
+	public void addListener(ArrivalListener l) {listeners.add(l);}
+	
+	/**Notify all items listening for aggregates here that a new one has arrived.**/
+	protected void notifyListeners() {
+		ArrivalEvent e = new ArrivalEvent(queue.size());
+		for (ArrivalListener l:listeners) {l.arrival(e);}
+	}
+	
+	/**Implement this interface to receive aggreate arrival events (and register implements vai addListener).**/
+	public static interface ArrivalListener extends java.util.EventListener {
+		public void arrival(ArrivalEvent e);
+	}
+	
+	/**Arrival event type.**/
+	public static class ArrivalEvent {
+		public final int waiting;
+		public ArrivalEvent(int waiting) {this.waiting = waiting;}
+	}
 
+
+	
 }
