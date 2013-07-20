ad48ce1652918e2950d0bfea7adc4691bb04ee3b
Joseph Cottam <jcottam@indiana.edu>
2013-07-20 18:26:59 -0400

    expanded config options.

--- AbstractRendering/ext/ar/ext/spark/Driver.java
+++ AbstractRendering/ext/ar/ext/spark/Driver.java
@@ -15,6 +15,14 @@ import ar.rules.Numbers;
 
 /**Main class for driving an ARSpark application.**/
 public class Driver {
+	private static String arg(String[] args, String flag, String def) {
+		flag = flag.toUpperCase();
+		for (int i=0; i<args.length; i++) {
+			if (args[i].toUpperCase().equals(flag)) {return args[i+1];}
+		}
+		return def;
+	}
+	
 	/* Get an RDD
 	 * 
 	 * Aggregates: FlatAggregates, probably wrapped up in something
@@ -27,15 +35,17 @@ public class Driver {
 	 **/
 	public static void main(String[] args){
 		if (args.length == 0) {
-			System.err.println("Usage: JavaTC <host> <data>");
+			System.err.println("Usage: JavaTC -host <host> -in <data> -out <out> -sh <spark-home> -jars <jars>");
 			System.exit(1);
 		}
 		
-		String host = args[0];
-		String path = args[1];
-		String outFile= args[2];
+		String host = arg(args, "-host", "localhost");
+		String path = arg(args, "-in", null);
+		String outFile= arg(args, "-out", null);
+		String sh= arg(args, "-sh", System.getenv("SPARK_HOME"));
+		String jars[] = arg(args, "-out", "").split(":");
 
-		JavaSparkContext sc = new JavaSparkContext(host, "SparkAbstractRendering");
+		JavaSparkContext sc = new JavaSparkContext(host, "SparkAbstractRendering",sh, jars);
 		JavaRDD<Indexed> base = sc.textFile(path).map(new StringToIndexed("\\s*,\\s*"));
 		Shaper<Indexed> shaper = new ToRect(.01, .01, false, 0, 1);
 		Valuer<Indexed,?> valuer = new ToValue(2);
