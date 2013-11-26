62682decdefb4c03bbf0c6e4c21eccf8d9e80ce7
Joseph Cottam <jcottam@indiana.edu>
2013-11-25 23:21:43 -0500

    Generalizing 'diamond' into 'fan'

--- java/core/ar/Aggregator.java
+++ java/core/ar/Aggregator.java
@@ -16,18 +16,13 @@ import java.io.Serializable;
 public interface Aggregator<IN,OUT> extends Serializable {
 	
 	/**
-	 * Compute an aggregate value from an existing aggregate value and a 
+	 * Compute an aggregate value from an existing aggregate values and a 
 	 * new input values.  If the return value is not equal-to "current,"
-	 * the return value must be a distinct object from "current" to ensure correct behavior.
+	 * the return value must be a distinct object from "current" to ensure correct behavior
+	 * (i.e., don't mutate current, even if its possible).
 	 * 
-	 * The x and y position are provided as arguments so position-sensitive
-	 * aggregation can be performed.  Any other contextual information
-	 * needs to be provided through the class in some other way.
+	 * Contextual information needs to be provided to the class through constructor (preferred) or property methods.
 	 *
-	 * TODO: With selectors in place...do we still need x/y?  If they are removed, then rollup and combine are the same!
-	 * 
-	 * @param x The x-position being operated on
-	 * @param y The y-position being operated on
 	 * @param current  An existing aggregate value
 	 * @param update A new input value
 	 * @return The aggregate value
