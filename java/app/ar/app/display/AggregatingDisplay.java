73950d6b540f7acd75081caab3627691cb770b45
Joseph Cottam <jcottam@indiana.edu>
2013-12-02 23:34:12 -0500

    Removed subset task.
    Does it in the full render or as a function call (it is just a wrapper now).

--- java/app/ar/app/display/AggregatingDisplay.java
+++ java/app/ar/app/display/AggregatingDisplay.java
@@ -34,7 +34,7 @@ public class AggregatingDisplay extends ARComponent.Aggregating {
 	protected volatile boolean subsetRender = false;
 	protected volatile boolean renderError = false;
 	protected volatile Aggregates<?> aggregates;
-	protected ExecutorService renderPool = new MostRecentOnlyExecutor(1,"FullDisplay Render Thread");//TODO: Redoing painting to use futures...
+	protected ExecutorService renderPool = new MostRecentOnlyExecutor(1,"FullDisplay Render Thread");
 		
 	protected final Renderer renderer;
 
@@ -111,25 +111,29 @@ public class AggregatingDisplay extends ARComponent.Aggregating {
 			g.fillRect(0, 0, this.getWidth(), this.getHeight());
  		} else if (fullRender) {
 			action = new AggregateRender();
+			renderPool.execute(action);
 			fullRender = false;
+			subsetRender = false;
 		} else if (subsetRender) {
-			action = new SubsetRender();
+			subsetAggregates();
 			subsetRender = false;
 		} 
 
-		if (action != null) {renderPool.execute(action);}
 	}
 		
 	/**Set the subset that will be sent to transfer.**/
-	public void subsetAggregates(Aggregates<?> aggregates) {
-		display.aggregates(aggregates,null);
+	public void subsetAggregates() {
+		Rectangle viewport = AggregatingDisplay.this.getBounds();				
+		AffineTransform vt = viewTransform();
+		int shiftX = (int) -(vt.getTranslateX()-renderTransform.getTranslateX());
+		int shiftY = (int) -(vt.getTranslateY()-renderTransform.getTranslateY());
+		Aggregates<?> subset = new SubsetWrapper<>(aggregates, shiftX, shiftY, shiftX+viewport.width, shiftY+viewport.height);
+
+		display.aggregates(subset,null);
 		repaint();
 	}
-
 	
 	public String toString() {return String.format("AggregatingDisplay[Dataset: %1$s, Transfer: %2$s]", dataset, display.transfer(), aggregator);}
-	
-	
 	
     /**Use this transform to convert values from the absolute system
      * to the screen system.
@@ -166,35 +170,6 @@ public class AggregatingDisplay extends ARComponent.Aggregating {
 	}
 	
 	public Rectangle2D dataBounds() {return dataset.bounds();}
-
-
-	
-	private final class SubsetRender implements Runnable {
-		public void run() {
-			long start = System.currentTimeMillis();
-			try {
-				Rectangle viewport = AggregatingDisplay.this.getBounds();				
-				AffineTransform vt = viewTransform();
-				int shiftX = (int) -(vt.getTranslateX()-renderTransform.getTranslateX());
-				int shiftY = (int) -(vt.getTranslateY()-renderTransform.getTranslateY());
-				
-				Aggregates<?> subset = new SubsetWrapper<>(aggregates, shiftX, shiftY, shiftX+viewport.width, shiftY+viewport.height);
-				AggregatingDisplay.this.subsetAggregates(subset);
-
-				long end = System.currentTimeMillis();
-				if (PERF_REP) {
-					System.out.printf("%d ms (Subset render)\n",
-							(end-start), subset.highX()-subset.lowX(), subset.highY()-subset.lowY());
-				}
-			} catch (ClassCastException e) {
-				renderError = true;
-				System.err.println(e.getMessage());
-				//e.printStackTrace();
-			}
-			
-			AggregatingDisplay.this.repaint();
-		}	
-	}
 	
 	private final class AggregateRender implements Runnable {
 		
@@ -216,6 +191,7 @@ public class AggregatingDisplay extends ARComponent.Aggregating {
 					System.out.printf("%d ms (Base aggregates render on %d x %d grid)\n",
 							(end-start), aggregates.highX()-aggregates.lowX(), aggregates.highY()-aggregates.lowY());
 				}
+				AggregatingDisplay.this.subsetAggregates();
 				
 			} catch (Exception e) {
 				renderError = true;
