d5aab950f5e56dcb9e3107c29ac8c3f39c0ca1a9
Joseph Cottam <jcottam@indiana.edu>
2014-01-30 15:55:16 -0500

    Restored presets-based demo app, but uses the Option system instead of the Wrapper system.
    ARComposerApp is now the 'extended' interface.

--- java/app/ar/app/ARComposerApp.java
+++ java/app/ar/app/ARComposerApp.java
@@ -3,64 +3,112 @@ package ar.app;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 
+import ar.Aggregates;
+import ar.aggregates.AggregateUtils;
 import ar.app.components.*;
+import ar.app.components.sequentialComposer.SequentialComposer;
 import ar.app.display.ARComponent;
 import ar.app.display.AggregatingDisplay;
 import ar.app.display.EnhanceHost;
 import ar.renderers.ParallelRenderer;
 import ar.renderers.RenderUtils;
+import ar.util.Util;
 
-public class ARDemoApp implements ARComponent.Holder, ar.util.HasViewTransform {
+
+//TODO: Add "subset input", useful for contours
+//TODO: Add "Specialize From Here"
+public class ARComposerApp implements ARComponent.Holder, ar.util.HasViewTransform {
 	private final EnhanceHost display = new EnhanceHost(new AggregatingDisplay(new ParallelRenderer()));
 	private final JFrame frame = new JFrame();
 
 	private final EnhanceOptions enhanceOptions = new EnhanceOptions();
-	private final Presets presets = new Presets(this);
+	private final JButton export = new JButton("Save Image");
 	private final Status status = new Status();
-
-	public ARDemoApp() {
+	private final SequentialComposer composer = new SequentialComposer(this);
+	
+	public ARComposerApp() {
 		ar.renderers.RenderUtils.RECORD_PROGRESS = true;
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Abstract Rendering (Demo App)");
 		frame.setLayout(new BorderLayout());
 		
-		JPanel controls = new JPanel(new GridLayout(0,1));
-		controls.add(enhanceOptions);
-		controls.add(presets);
-		controls.add(status);
-		controls.add(new JLabel("Double-click zooms to fit.", JLabel.CENTER));
+		JPanel topRow = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill =  GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth =1;
+		c.weightx = 1;
+		topRow.add(enhanceOptions,c);
 
-		frame.add(controls, BorderLayout.SOUTH);
+		c.fill =  GridBagConstraints.HORIZONTAL;
+		c.gridx = 2;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.weightx = 1;
+		topRow.add(status,c);
 		
-		
-		this.status.startMonitoring(display.renderer());
-		enhanceOptions.host(display);
-
-		
-		final ARDemoApp app = this;
-		presets.addActionListener(new ActionListener() {
+		c.fill =  GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.weightx = 1;
+		topRow.add(export, c);
+		export.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				boolean rezoom = presets.doZoomWith(app.display);
-				presets.update(app.display);
+				JFileChooser chooser = new JFileChooser();
+				ARComponent arc = ARComposerApp.this.getARComponent();
+				int rv = chooser.showSaveDialog(arc);
+				if (rv != JFileChooser.APPROVE_OPTION) {return;}
+				File file = chooser.getSelectedFile();
+	
+				@SuppressWarnings("unchecked") //Will fail if the transfer didn't end up with colors
+				BufferedImage img = AggregateUtils.asImage((Aggregates<Color>) arc.transferAggregates(), arc.getWidth(), arc.getHeight(), Color.white);
+				Util.writeImage(img, file);
+			}
+		});
+		
+				
+
+		JPanel controls = new JPanel();
+		controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
+		controls.add(topRow);
+		controls.add(composer);
+		
+		frame.add(controls, BorderLayout.SOUTH);
+
+		
+
+		final ARComposerApp app = this;
+		composer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				boolean rezoom = composer.doZoomWith(app.display);
+				update(app.display);
 				if (rezoom) {
 					display.zoomFit();
 				}
 			}
 		});
 		
-		presets.update(app.display);
+		update(app.display);
 		
 		frame.add(display, BorderLayout.CENTER);
 
+
+		frame.setLocation(200, 0);
 		frame.setSize(800, 800);
 		frame.validate();
 		frame.setVisible(true);
@@ -75,6 +123,20 @@ public class ARDemoApp implements ARComponent.Holder, ar.util.HasViewTransform {
 				}
 			);
 		} catch (InvocationTargetException | InterruptedException e1) {}
+	
+		//Plumbing for the top row...
+		this.status.startMonitoring(display.renderer());
+        enhanceOptions.host(display);	
+    }
+	
+	public void update(ARComponent.Aggregating panel) {
+		if (panel.dataset() == composer.dataset()
+			&& panel.aggregator().equals(composer.aggregator())) {
+		    panel.transfer(composer.transfer());
+		} else {
+			panel.dataset(composer.dataset(), composer.aggregator(), composer.transfer());
+			panel.zoomFit();
+		}
 	}
 	
 	public static <A,B> void loadInstances(JComboBox<B> target, Class<A> source) {
@@ -112,10 +174,6 @@ public class ARDemoApp implements ARComponent.Holder, ar.util.HasViewTransform {
 		ARComponent.PERF_REP = true;
 		RenderUtils.RECORD_PROGRESS = true;
 		RenderUtils.REPORT_STEP=1_000_000;
-		if (args.length >0 && args[0]!=null && args[0].toUpperCase().equals("-EXT")) {
-			ARComposerApp.main(args);
-		} else {
-			new ARDemoApp();
-		}
+		new ARComposerApp();
 	} 
 }
