07cc7fb59052dc82005bbee4bd08b284c0a896b1
Joseph Cottam <jcottam@indiana.edu>
2014-02-02 01:25:42 -0500

    Unused imports...

--- java/app/ar/app/components/sequentialComposer/TransferBuilder.java
+++ java/app/ar/app/components/sequentialComposer/TransferBuilder.java
@@ -3,7 +3,6 @@ package ar.app.components.sequentialComposer;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
