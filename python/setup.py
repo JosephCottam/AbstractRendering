02ca3b553ac8ef130cde62c71a3aa5a65049176e
Joseph Cottam <jcottam@indiana.edu>
2014-06-23 13:41:52 -0400

    Conditional compilation for libdispatch version...perhaps

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,24 @@
 from distutils.core import setup, Extension
+import sys
+
+### Flag to indicate that libdispatch should be used (OS X only)
+DISPATCH_FLAG = "--dispatch"
+
+if DISPATCH_FLAG in sys.argv:
+  transform = Extension('abstract_rendering.transform_libdispatch',
+                          ['transform_libdispatch.cpp'], 
+                           extra_compile_args=['-std=c++11','-O3', '-Wall', '-march=native', '-fno-rtti', '-fno-exceptions', '-fPIC', '-lstdc++'])
+  del sys.argv[sys.argv.index(DISPATCH_FLAG)]
+
+else:
+  transform = Extension('abstract_rendering.transform',
+                             ['transform.cpp'],
+                             extra_compile_args=['-std=c++11','-O3', '-Wall', '-march=native', '-fno-rtti', '-fno-exceptions', '-fPIC', '-lstdc++'])
+
+
 
 setup(name='abstract_rendering', 
-      version='0.0.1',
+      version='0.0.2',
       description='Rendering as a binning process',
       author='Joseph Cottam',
       author_email='jcottam@indiana.edu',
@@ -15,11 +32,7 @@ setup(name='abstract_rendering',
                   'abstract_rendering.glyphset', 
                   'abstract_rendering.infos',
                   'abstract_rendering.numeric'],
-      ext_modules=[Extension('abstract_rendering.transform',
-                             ['transform.cpp'],
-                             extra_compile_args=['-std=c++11','-O3', '-Wall', '-march=native', '-fno-rtti', '-fno-exceptions', '-fPIC', '-lstdc++']),
-                   Extension('abstract_rendering.transform_libdispatch',
-                             ['transform_libdispatch.cpp'], 
-                             extra_compile_args=['-std=c++11','-O3', '-Wall', '-march=native', '-fno-rtti', '-fno-exceptions', '-fPIC', '-lstdc++'])]
-     )
-      
+      ext_modules=[transform],
+      classifiers=["Development Status :: 3 - Alpha", "Intended Audience :: Science/Research", "Topic :: Scientific/Engineering :: Visualization"]
+
+      )
