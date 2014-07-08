2b1e74d5b00210c63488ca85d0053da06f340b83
Joseph Cottam <jcottam@indiana.edu>
2014-07-08 17:25:32 -0400

    Setting 0.1 version number

--- python/setup.py
+++ python/setup.py
@@ -121,7 +121,7 @@ if 'develop' in sys.argv:
     sys.exit()
 
 setup(name='abstract_rendering', 
-      version='0.0.3',
+      version='0.1.0',
       description='Rendering as a binning process',
       author='Joseph Cottam',
       author_email='jcottam@indiana.edu',
