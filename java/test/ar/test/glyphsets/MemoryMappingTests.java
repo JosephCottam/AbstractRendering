12c3fcb6a9268e5b89323e2530da8eeb164baedd
Joseph Cottam <jcottam@indiana.edu>
2014-07-11 00:30:28 -0400

    Increasing visibliity of file structure on travis-ci

--- java/test/ar/test/glyphsets/MemoryMappingTests.java
+++ java/test/ar/test/glyphsets/MemoryMappingTests.java
@@ -26,11 +26,31 @@ public class MemoryMappingTests {
 	private static Glyphset.RandomAccess<Rectangle2D, Integer> mm = null;
 	private static Glyphset.RandomAccess<Rectangle2D, Integer> ref = null;
 	
+	private static void list(File folder) {
+		File[] listOfFiles = folder.listFiles();
+
+	    for (int i = 0; i < listOfFiles.length; i++) {
+	      if (listOfFiles[i].isFile()) {
+	        System.out.println("File " + listOfFiles[i].getName());
+	      } else if (listOfFiles[i].isDirectory()) {
+	        System.out.println("Directory " + listOfFiles[i].getName());
+	      }
+	    }
+	}
+	
 	@BeforeClass
 	public static void makeHbin() throws Exception {
 		File csv = new File(csvName);
 		File hbin = new File(hbinName);
 		
+		System.out.println("---------------------------------------");
+		System.out.println("---------------------------------------");
+		list(csv.getParentFile().getParentFile());
+		System.out.println("---------------------------------------");
+		list(csv.getParentFile());
+		System.out.println("---------------------------------------");
+		System.out.println("---------------------------------------");
+				
 		assertTrue(String.format("Source file not found: %s\n Referred to as %s", csv.getCanonicalPath(), csvName), csv.exists());
 		if (hbin.exists()) {hbin.delete();}
 		MemMapEncoder.write(csv, 1, hbin, "xxddi".toCharArray());
