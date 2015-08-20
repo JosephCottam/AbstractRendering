c9a6806cd1d3499a53ba36ded4bbf0fb2bc31020
Joseph Cottam <jcottam@indiana.edu>
2015-08-20 16:08:00 -0400

    Atom reify working.

--- java/ext/ar/ext/lang/Parser.java
+++ java/ext/ar/ext/lang/Parser.java
@@ -1,38 +1,59 @@
 package ar.ext.lang;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-
-import com.google.common.base.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.*;
 
 public class Parser {
-	public class TreeNode<A> implements Iterable<TreeNode<A>> {
+	public static class TreeNode<A> implements Iterable<TreeNode<A>> {
 		  private Optional<A> value;
-		  private Set<TreeNode<A>> children;
+		  private List<TreeNode<A>> children;
 		  
-		  public TreeNode() {this(null, Collections.emptySet());}
-		  public TreeNode(A value) {this(value, Collections.emptySet());}
-		  public TreeNode(A value, Set<TreeNode<A>> children) {
-			  this.value = Optional.fromNullable(value);
-			  this.children = Collections.unmodifiableSet(children);
+		  public TreeNode(Optional<A> value, List<TreeNode<A>> children) {
+			  this.value = value;
+			  this.children = children;
 		  }
 		  
 		  public Iterator<TreeNode<A>> iterator() {return children.iterator();}
-		  public Set<TreeNode<A>> children() {return children;}
+		  public List<TreeNode<A>> children() {return children;}
 		  public Optional<A> value() {return value;}
 		  
 		  public TreeNode<A> extend(TreeNode<A> child) {
-			  Set<TreeNode<A>> children = new HashSet<>();
-			  children.addAll(children);
-			  children.add(child);
-			  return new TreeNode<>(null, children);
+			  List<TreeNode<A>> newChildren = new ArrayList<>();
+			  newChildren.addAll(children);
+			  newChildren.add(child);
+			  return new TreeNode<A>(value, newChildren);
 		  }
+		  
+		  public boolean equals(Object alter) {
+			  if (!(alter instanceof TreeNode)) {return false;}
+			  TreeNode<?> other = (TreeNode<?>) alter;
+			  return this.value.equals(other.value)
+					  && this.children.size() == other.children.size()
+					  && IntStream.range(0,children.size())
+					  		.allMatch(i -> children.get(i).equals(other.children.get(i)));
+		  }
+		  
+		  public String toString() {
+			  String val = value.isPresent() ? value.get().toString() : "<null>";
+			  String start = children.size() > 0 ? "(" : "";
+			  String end = children.size() > 0 ? ")" : "";
+			  return start +  val + " " + children.stream().map(e -> e.toString()).collect(Collectors.joining(" ")) + end;
+		  }
+		  
+		  public static <A> TreeNode<A> empty() {return new TreeNode<A>(Optional.empty(), Collections.emptyList());}
+		  public static <A> TreeNode<A> leaf(A value) {return new TreeNode<A>(Optional.ofNullable(value), Collections.emptyList());}
+
+		  @SafeVarargs
+		  public static <A> TreeNode<A> inner(TreeNode<A>... children) {return new TreeNode<A>(Optional.empty(), Arrays.asList(children));}
+
 	}
 	
 	public static List<String> tokens(String input) {
@@ -41,16 +62,49 @@ public class Parser {
 				 .collect(toList());
 	}
 	
-	public TreeNode<String> parseTree(Iterator<String> tokens) {
-		TreeNode<String> root = new TreeNode<>();
+	public static TreeNode<String> parseTree(Iterable<String> tokens) {
+		TreeNode<String> result = parseTree(tokens.iterator(), TreeNode.empty());
+		return result.children.get(0);
+	}
+	
+	public static TreeNode<String> parseTree(Iterator<String> tokens, TreeNode<String> root) {
 		while (tokens.hasNext()) {
 			String token = tokens.next();
 			switch (token) {
-				case "(": root = root.extend(parseTree(tokens)); break;
-				case ")": return root;
-				default: root = root.extend(new TreeNode<>(token)); break;
+				case "(": 
+					TreeNode<String> newRoot = TreeNode.empty();
+					root = root.extend(parseTree(tokens, newRoot)); 
+					break;
+				case ")": 
+					return root;
+				default: 
+					root = root.extend(TreeNode.leaf(token)); 
+					break;
 			}
 		}		
 		return root;
 	}
+	
+	public static Object reify(TreeNode<?> tree) {
+		if (!tree.value().isPresent()) {
+			Object[] parts = tree.children().stream().map(e -> reify(e)).toArray();
+			String fn;
+			if (parts[0] instanceof String) {fn = (String) parts[0];}
+			else {throw new IllegalArgumentException("Must have string in first position");}
+			
+			
+			
+			throw new UnsupportedOperationException("Recursive case not implemented");
+		}
+		
+		String val = tree.value().get().toString();
+		try {return Integer.parseInt(val);}
+		catch (Exception e) {
+			try {return Double.parseDouble(val);}
+			catch (Exception ex) {return val;}
+		}
+		
+	}
+	
+	public static TreeNode<String> parse(String input) {return parseTree(tokens(input));}
 }
--- java/test/ar/test/ext/LangTest.java
+++ java/test/ar/test/ext/LangTest.java
@@ -2,18 +2,42 @@ package ar.test.ext;
 
 import static org.junit.Assert.*;
 
+import org.fusesource.jansi.Ansi.Color;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
-import ar.ext.lang.Parser;
+import static ar.ext.lang.Parser.*;
+import static ar.ext.lang.Parser.TreeNode.*;
 
 public class LangTest {
 	@Test
 	public void tokenize() {
-		assertThat(Parser.tokens("test"), contains("test"));
-		assertThat(Parser.tokens("(test)"), contains("(", "test", ")"));
-		assertThat(Parser.tokens("(test of some more)"), contains("(", "test", "of", "some", "more", ")"));
-		assertThat(Parser.tokens("((nested))"), contains("(", "(", "nested", ")", ")"));
-		assertThat(Parser.tokens("((nested (deeply)) shallowly)"), contains("(", "(", "nested", "(", "deeply", ")", ")", "shallowly", ")"));
+		assertThat(tokens("test"), contains("test"));
+		assertThat(tokens("(test)"), contains("(", "test", ")"));
+		assertThat(tokens("(test of some more)"), contains("(", "test", "of", "some", "more", ")"));
+		assertThat(tokens("((nested))"), contains("(", "(", "nested", ")", ")"));
+		assertThat(tokens("((nested (deeply)) shallowly)"), contains("(", "(", "nested", "(", "deeply", ")", ")", "shallowly", ")"));
+	}
+	
+	@Test
+	public void tree() {
+		assertThat(parse("test"), is(equalTo(leaf("test"))));
+		assertThat(parse("(test)"), is(equalTo(inner(leaf("test")))));
+		assertThat(parse("(test of some more)"), is(equalTo(inner(leaf("test"), leaf("of"), leaf("some"), leaf("more")))));
+		assertThat(parse("(nested)"), is(equalTo(inner(leaf("nested")))));
+		assertThat(parse("((nested (deeply)) shallowly)"), is(equalTo(inner(inner(leaf("nested"), inner(leaf("deeply"))), leaf("shallowly")))));
+	}
+	
+	@Test
+	public void reifyAtom() {
+		assertThat(reify(parse("1")), is(Integer.valueOf(1)));
+		assertThat(reify(parse("1.3")), is(Double.valueOf(1.3)));
+		assertThat(reify(parse("1.3b")), is("1.3b"));
+		assertThat(reify(parse("hello")), is("hello"));
+	}
+	
+	@Test
+	public void reifyNested() {
+		assertThat(reify(parse("(rgb 0 0 0)")), is(Color.BLACK));
 	}
 }
