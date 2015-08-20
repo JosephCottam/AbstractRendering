22ae00e38ef19624e385a6cf9c5bd584b1b788c6
Joseph Cottam <jcottam@indiana.edu>
2015-08-20 13:53:37 -0400

    Removed warnings

--- java/ext/ar/ext/server/ARServer.java
+++ java/ext/ar/ext/server/ARServer.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
-import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.AffineTransformOp;
